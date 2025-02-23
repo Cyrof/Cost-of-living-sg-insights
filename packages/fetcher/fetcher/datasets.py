cef17af69f21b5fd2d187a1160ad211e73184af3
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-02-23 21:36:23 +0800

    fix(fetcher, data): remove `_id` postfix from dataset paths

--- packages/fetcher/fetcher/datasets.py
+++ packages/fetcher/fetcher/datasets.py
@@ -19,21 +19,21 @@ DEFAULT_DATASETS: dict[str, Dataset] = {
     "global_gdp": Dataset(Source.WorldBank, "NY.GDP.PCAP.PP.KD"),
     "global_reer2010": Dataset(Source.WorldBank, "PX.REX.REER"),
     "sg_cpi_annual": Dataset(Source.DataGov, "d_dcb352661fb449c4a4c0ab23aa8d6399"),
-    "sg_iras_tax_id": Dataset(Source.DataGov, "d_21e22578cabce897e8b27801e5596140"),
-    "sg_property_tax_rates_id": Dataset(
+    "sg_iras_tax": Dataset(Source.DataGov, "d_21e22578cabce897e8b27801e5596140"),
+    "sg_property_tax_rates": Dataset(
         Source.DataGov, "d_2109ad1eafff52dab388f9bcd8148a35"
     ),
-    "sg_hdb_annual_value_tax_id": Dataset(
+    "sg_hdb_annual_value_tax": Dataset(
         Source.DataGov, "d_48143be392f1ed22f0700835212e5a60"
     ),
-    "sg_gst_id": Dataset(Source.DataGov, "d_2e65ed309aa8d449d1bd0c7ef7c7e4da"),
-    "sg_income_tax_rates_id": Dataset(
+    "sg_gst": Dataset(Source.DataGov, "d_2e65ed309aa8d449d1bd0c7ef7c7e4da"),
+    "sg_income_tax_rates": Dataset(
         Source.DataGov, "d_f73055c69144d2e7734c28811d3982aa"
     ),
-    "sg_individual_income_tax_id": Dataset(
+    "sg_individual_income_tax": Dataset(
         Source.DataGov, "d_f394f202534237671d39b17bd3b506ec"
     ),
-    "sg_gross_income_id": Dataset(Source.DataGov, "d_52760e82e8786bac11cca40eb29d1a93"),
-    "sg_expenditure_id": Dataset(Source.SingStat, "D10072"),
-    "sg_life_expectancy_id": Dataset(Source.SingStat, "M810501"),
+    "sg_gross_income": Dataset(Source.DataGov, "d_52760e82e8786bac11cca40eb29d1a93"),
+    "sg_expenditure": Dataset(Source.SingStat, "D10072"),
+    "sg_life_expectancy": Dataset(Source.SingStat, "M810501"),
 }
