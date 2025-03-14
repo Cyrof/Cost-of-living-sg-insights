7a2a47f038c14eeea2194783364bf426df92eabf
keith <keithneo00@gmail.com>
2025-03-14 21:50:42 +0800

    refactor(healthcare): Done up the placeholder and styling for this page

--- dashboard/pages/healthcare.py
+++ dashboard/pages/healthcare.py
@@ -2,6 +2,8 @@ import dash
 from dash import dcc, html
 from dash.development.base_component import Component
 from plotly.graph_objects import Figure
+import dash_mantine_components as dmc
+from components.graphWrapper import graphWrapper
 
 import dashboard.utils
 
@@ -27,25 +29,68 @@ def load_charts() -> dict[str, Figure]:
 
 def layout() -> Component:
     charts: dict[str, Figure] = load_charts()
-
-    return html.Div(
+    return dmc.Stack(
         [
-            html.H1("Healthcare"),
-            dcc.Graph(
-                id="life_expectancy_vs_healthcare_cpi_chart",
-                figure=charts["life_expectancy_vs_healthcare_cpi"],
+            dmc.Stack(
+                [
+                    dmc.Text(
+                        "Necessities",
+                        className="text-3xl font-semibold"
+                    ),
+                    dmc.Group(
+                        gap="lg",
+                        grow=True,
+                        children=[
+                            dmc.Text(
+                                "Healthcare Components text",
+                                className="border border-purple-500"
+                            ),
+                            graphWrapper(
+                                id="healthcare_cpi_breakdown_chart",
+                                figure=charts["healthcare_cpi_breakdown"]
+                            )
+                        ]
+                    )
+                ]
             ),
-            dcc.Graph(
-                id="healthcare_cpi_vs_gross_monthly_income_chart",
-                figure=charts["healthcare_cpi_vs_gross_monthly_income"],
+            dmc.SimpleGrid(
+                cols=2,
+                spacing="lg",
+                children=[
+                    graphWrapper(
+                        id="life_expectancy_vs_healthcare_cpi_chart",
+                        figure=charts["life_expectancy_vs_healthcare_cpi"]
+                    ),
+                    graphWrapper(
+                        id="healthcare_cpi_vs_gross_monthly_income",
+                        figure=charts["life_expectancy_vs_healthcare_cpi"]
+                    ),
+                    dmc.Text(
+                        "Life Expectancy vs Healthcare CPI text",
+                        className="border border-orange-500"
+                    ),
+                    dmc.Text(
+                        "Healthcare CPI vs Income Growth (%) text",
+                        className="border border-green-500"
+                    )
+                ],
+                className="mt-12"
             ),
-            dcc.Graph(
-                id="percentage_change_in_healthcare_cpi_and_income_chart",
-                figure=charts["percentage_change_in_healthcare_cpi_and_income"],
-            ),
-            dcc.Graph(
-                id="healthcare_cpi_breakdown_chart",
-                figure=charts["healthcare_cpi_breakdown"],
-            ),
-        ]
+            dmc.Group(
+                gap="lg",
+                grow=True,
+                children=[
+                    graphWrapper(
+                        id="percentage_change_in_healthcare_cpi_and_income_chart",
+                        figure=charts["percentage_change_in_healthcare_cpi_and_income"]
+                    ),
+                    dmc.Text(
+                        "Healthcare CPI vs Income Growth",
+                        className="border border-blue-500"
+                    )
+                ],
+                className="mt-12"
+            )
+        ],
+        className="py-2 px-4"
     )
