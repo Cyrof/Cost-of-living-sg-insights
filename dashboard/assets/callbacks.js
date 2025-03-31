b1d61c1a3e08136071bf76503e651e0ac36bfa6c
keith <keithneo00@gmail.com>
2025-03-31 18:43:24 +0800

    refactor(callbacks.js, app_shell.py): Added an overlay and fix some issue where the sidebar does not close on page render

--- dashboard/assets/callbacks.js
+++ dashboard/assets/callbacks.js
@@ -1,25 +1,48 @@
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
-        toggleSidebar: function(mobileOpened, desktopOpened, appShellNavbar) {
-            appShellNavbar["collapsed"] = {
-                mobile: !mobileOpened,
-                desktop: !desktopOpened,
-            };
-            console.log(mobileOpened);
-            console.log(desktopOpened);
-            console.log(appShellNavbar);
-            return appShellNavbar;
+        updateSidebarState: function(mobileOpened, desktopOpened, pathname, overlayClicks, appShellNavbar){
+            var ctx = dash_clientside.callback_context;
+            var trigger = ctx.triggered[0].prop_id;
+            var resetBurgers = false
+
+            // url change or overlay clicked, close sidebar
+            if(trigger.includes("url.pathname") || trigger.includes("sidebar-overlay.n_clicks")){
+                appShellNavbar["collapsed"] = { mobile: true, desktop: true };
+                resetBurgers = true;
+                return [
+                    appShellNavbar, 
+                    { "display": "none" },
+                    resetBurgers ? false : mobileOpened,
+                    resetBurgers ? false : desktopOpened
+                ];
+            } else {
+                // toggle based on burger state
+                var collapsed = { mobile: !mobileOpened, desktop: !desktopOpened };
+                appShellNavbar["collapsed"] = collapsed;
+
+                // show overlay is sidebar open
+                var isOpen = (!collapsed.mobile || !collapsed.desktop);
+                var overlayStyle = isOpen ? {
+                    "display": "block",
+                } : {
+                    "display": "none"
+                };
+                return [
+                    appShellNavbar, 
+                    overlayStyle,
+                    mobileOpened,
+                    desktopOpened
+                ];
+            }
         },
         setActiveNavLink: function(pathname, ...hrefs) {
             return hrefs.map((href) => href === pathname);
         },
         toggleCard: function(nClicks) {
-            // Toggle based on the click count: odd -> open, even -> closed.
             if (nClicks && nClicks % 2 === 1) {
                 return [true, "Show Less"];
             }
-
             return [false, "Read More"];
-        },
-    },
-});
+        }
+    }
+})
